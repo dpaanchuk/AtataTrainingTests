489c4f143d65e9eef108db20c583bed712ae2410
Denys Panchuk <dpaanchuk@gmail.com>
2025-10-21 14:48:19 +0300

    Add project files.

---
+++ AtataTrainingTests/UITestFixture.cs
@@ -0,0 +1,14 @@
+﻿namespace AtataTrainingTests
+{
+    [Parallelizable(ParallelScope.Self)]
+    public abstract class UITestFixture
+    {
+        [SetUp]
+        public void SetUp() =>
+            AtataContext.Configure().Build();
+
+        [TearDown]
+        public void TearDown() =>
+            AtataContext.Current?.Dispose();
+    }
+}
